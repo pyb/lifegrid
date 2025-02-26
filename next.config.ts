0293e4744d511a4647d04a93070a266a41ee16bb
pyb <pyb@pyb.me.uk>
2025-02-26 13:23:04 -0500

    Template config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    //ignoreDuringBuilds: true,
+  },
+  experimental: {
+  },
+  devIndicators: {
+    appIsrStatus: false,
+  },
 };
 
 export default nextConfig;
---
+++ src/app/Game.tsx
@@ -0,0 +1,9 @@
+const Game = () => {
+    return (
+        <div>
+            Foo
+        </div>
+    );
+}
+
+export default Game;
