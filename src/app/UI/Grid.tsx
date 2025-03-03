42fb74d45c329344a8c437abb276c9fbc8d2c547
pyb <pyb@pyb.me.uk>
2025-03-03 17:08:01 -0500

    Grid CSS (mostly copied from old version)

--- src/app/UI/Grid.tsx
+++ src/app/UI/Grid.tsx
@@ -20,7 +20,6 @@ const GridItem = ({name, text, hoverText, progress, style, onClick}:GridItemProp
     );
 }
 
-
 interface TaskItemProps {
     name: string,
     task: number,
@@ -57,31 +56,47 @@ interface GridProps {
 };
 
 const itemList = (side: number, gs: GameState) => {
-    const dollar = gs.resources.get(Item.Dollar);
-    //const work 
-
     if (side == 0)
-        return <>
-            <ResourceItem name="Dollar" resource={Item.Dollar} gs={gs} />
-            <ResourceItem name="Farm" resource={Item.Farm} gs={gs} />
-            <ResourceItem name="Farm" resource={Item.Farm} gs={gs} />
-            <ResourceItem name="Farm" resource={Item.Farm} gs={gs} />
-            <ResourceItem name="Farm" resource={Item.Farm} gs={gs} />
-        </>
+        return [
+            <ResourceItem name="Dollar" resource={Item.Dollar} gs={gs} />,
+            <ResourceItem name="Farm" resource={Item.Farm} gs={gs} />,
+            <ResourceItem name="Farm" resource={Item.Farm} gs={gs} />,
+            <ResourceItem name="Farm" resource={Item.Farm} gs={gs} />,
+            <ResourceItem name="Farm" resource={Item.Farm} gs={gs} />,
+        ];
     else
-        return <>
-            <TaskItem name="Work" task={Item.Work} gs={gs}/>
-            <TaskItem name="Build" task={Item.Build} gs={gs}/>
-            <TaskItem name="Level" task={Item.Lettuce} gs={gs}/>
-        </>
+        return [
+            <TaskItem name="Work" task={Item.Work} gs={gs}/>,
+            <TaskItem name="Build" task={Item.Build} gs={gs}/>,
+            <TaskItem name="Level" task={Item.Lettuce} gs={gs}/>,
+    ];
+}
+
+const ResourceGrid = ({items}:{items:Array<React.ReactNode>}) => {
+    return (
+        <div className={styles.resourceGrid}>
+            <div className={styles.resourceRow}>
+                {items.slice(0,3)}
+            </div>
+            <div className={styles.resourceRow}>
+                {items.slice(3)}
+            </div>
+        </div>
+    );
+}
+
+const TaskGrid = ({items}:{items:Array<React.ReactNode>}) => {
+    return (
+        <div className={styles.taskGrid}>
+           {items}
+        </div>
+    );
 }
 
 const Grid = ({side, gs}:GridProps) => {
-    return (
-        <div className={side == 0 ? styles.resourceGrid :  styles.taskGrid}>
-           {itemList(side, gs)}
-        </div>
-    );
+  return (side == 0 ? 
+    <ResourceGrid items={itemList(side, gs)} /> :
+    <TaskGrid items={itemList(side, gs)} /> );
 }
 
 export default Grid;
